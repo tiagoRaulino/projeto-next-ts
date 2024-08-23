2e3bb79c8a07c7493c12a3b4951f9f1acc7051ce
tiagoRaulino <tiagoraulino542@gmail.com>
2024-08-23 03:55:56 -0300

    primeiro commit

---
+++ app/components/Table.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Link from 'next/link'
+
+function Table() {
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <td colpan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Searchbar from './components/Searchbar'
+import Table from './components/Table'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Searchbar/>
+      <Table/>
+    </div>
+  )
+}
+
+export default Home
